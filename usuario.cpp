f7caaad9120c537c0c8df028705f95a934073357
Bianca Dias <bianca.dias@ufv.br>
2022-02-04 15:14:12 -0300

    Inclusão dos arquivos Usuario.h e Usuario.cpp

---
+++ usuario.cpp
@@ -0,0 +1,8 @@
+#include "usuario.h"
+#include <iostream>
+
+Usuario::Usuario((std::string nome, int cpf)
+{
+    _nome = nome;
+    _cpf = cpf;
+}
---
+++ usuario.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+Class Usuario
+{
+    protected:
+        std::string _nome;
+        int _cpf;
+    public:
+        Usuario(std::string nome, int cpf);
+
+
+};
