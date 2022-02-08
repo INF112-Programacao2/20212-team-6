74d84ba03ed90a4fa58a95377bb699c242fbbdff
Bianca Dias <bianca.dias@ufv.br>
2022-02-08 11:21:15 -0300

    alterações na classe produto

--- produto.cpp
+++ produto.cpp
@@ -1,8 +1,9 @@
 #include "produto.h"
 
-Produto::Produto(int id, double preco){
+Produto::Produto(int id, double preco, int quantidade){
     _id = id;
     _preco = preco;
+    _quantidade = quantidade;
 }
 
 int Produto::getID(){
@@ -11,4 +12,19 @@ int Produto::getID(){
 
 double Produto::getPreco(){
     return _preco;
+}
+
+int Produto::getQuantidade(){
+    return _quantidade;
+}
+
+bool Produto::getEmEstoque(){
+    return _emEstoque;
+}
+
+void Produto::setEmEstoque(){
+    if(_quantidade!=0)
+        _emEstoque=true;
+    else
+        _emEstoque=false;
 }
--- produto.h
+++ produto.h
@@ -2,10 +2,17 @@ class Produto {
     private:
     int _id;
     double _preco;
+    int _quantidade;
+    bool _emEstoque;
 
     public:
-    Produto(int id, double preco);
+    Produto(int id, double preco, int quantidade);
 
     int getID();
     double getPreco();
+    int getQuantidade();
+    bool getEmEstoque();
+
+    void setEmEstoque();
+
 };
