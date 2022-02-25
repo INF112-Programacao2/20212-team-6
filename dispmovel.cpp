66afd2da6ddb0e97e9eb25ac5d3d1f34ef2c1356
Daniel Rosado <daniel.monteirorosado@gmail.com>
2022-02-25 17:47:47 -0300

    adicionando classe dispmovel .h e .cpp

---
+++ dispmovel.cpp
@@ -0,0 +1,13 @@
+#include "dispmovel.h"
+
+int DispMovel::getArmazenamento(){
+    return _armazenamento;
+}
+
+int DispMovel::getRam(){
+    return _ram;
+}
+
+int DispMovel::getTela(){
+    return _tela;
+}
