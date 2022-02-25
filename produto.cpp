ce509b709c49c7ac6800e48dec9d1d1c12f0fbe3
Daniel Rosado <daniel.monteirorosado@gmail.com>
2022-02-25 17:36:30 -0300

    adionando string marca e metodos

--- produto.cpp
+++ produto.cpp
@@ -4,6 +4,7 @@ Produto::Produto()
 {
 
 }
+
 Produto::Produto(int id, double preco, int quantidade) {
     _id = id;
     _preco = preco;
@@ -26,9 +27,17 @@ bool Produto::getEmEstoque() {
     return _emEstoque;
 }
 
+std::string Produto::getMarca() {
+    return _marca;
+}
+
 void Produto::setEmEstoque() {
     if (_quantidade != 0)
         _emEstoque = true;
     else
         _emEstoque = false;
+}
+
+void Produto::setMarca(std::string marca) {
+    _marca = marca;
 }
--- produto.h
+++ produto.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 class Produto {
 private:
     int _id;
@@ -5,15 +7,24 @@ private:
     int _quantidade;
     bool _emEstoque;
 
+    std::string _marca;
+
 public:
+    //Construtor
     Produto();
     Produto(int id, double preco, int quantidade);
 
+    //Gets
     int getID();
     double getPreco();
     int getQuantidade();
     bool getEmEstoque();
 
+    std::string getMarca();
+
+    //Sets
     void setEmEstoque();
+
+    void setMarca(std::string marca);
 
 };
