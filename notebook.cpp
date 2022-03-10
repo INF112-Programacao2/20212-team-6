891bd95c0917d7e6f58c9343c681f378827bc5a1
Daniel Rosado <daniel.monteirorosado@gmail.com>
2022-03-10 16:49:37 -0300

    construtor e set (classe notebook)

--- notebook.cpp
+++ notebook.cpp
@@ -1,5 +1,8 @@
 #include "notebook.h"
 
+Notebook::Notebook(int id, double preco, int quantidade, std::string marca, int tela, int ram, int armazenamento):
+    Produto(id, preco, quantidade, marca), _tela(tela), _ram(ram), _armazenamento(armazenamento) {}
+
 int Notebook::getArmazenamento(){
     return _armazenamento;
 }
@@ -10,4 +13,16 @@ int Notebook::getRam(){
 
 int Notebook::getTela(){
     return _tela;
+}
+
+void Notebook::setArmazenamento(int armazenamento){
+    _armazenamento = armazenamento;
+}
+
+void Notebook::setRam(int ram){
+    _ram = ram;
+}
+
+void Notebook::setTela(int tela){
+    _tela = tela;
 }
