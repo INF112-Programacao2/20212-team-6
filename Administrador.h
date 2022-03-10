a651ef9e2f91245a664dcd34193e7997ae1392ba
Ana Laura <80439129+analauratvrs@users.noreply.github.com>
2022-03-09 22:25:40 -0300

    instanciando objeto da classe Estoque

--- Administrador.h
+++ Administrador.h
@@ -5,15 +5,13 @@
 
 #include "usuario.h"
 #include "Produto.h"
+#include "Estoque.h"
 
-const int tamanho = 20;         // usada no tamanho do estoque;
-
-class Admin : public Usuario {
+class Administrador : public Usuario {
     protected:
-        Produto Estoque[tamanho];       // estoque de teste
+        Estoque EstoqueDaLoja;       // estoque
     public:
         Admin(std::string nome, int cpf);
-        static int indiceDoEstoque;            // controla o indice do estoque para quando novos produtos forem adicionados
         void menuAdm();
         void adicionarProduto();
         void excluirProduto();                 // será implementada quando houver o estoque real
