ba0ff835caec1fd6e1d2a2ab0574b23cf9da3e14
Daniel Rosado <daniel.monteirorosado@gmail.com>
2022-03-10 16:49:46 -0300

    construtor e set (classe televisores)

--- televisores.cpp
+++ televisores.cpp
@@ -1,9 +1,20 @@
 #include "televisores.h"
 
+Televisores::Televisores(int id, double preco, int quantidade, std::string marca, int tela, int resolucao):
+    Produto(id, preco, quantidade, marca), _tela(tela), _resolucao(resolucao) {}
+
 int Televisores::getTela(){
     return _tela;
 }
 
 int Televisores::getResolucao(){
     return _resolucao;
+}
+
+void Televisores::setTela(int tela){
+    _tela = tela;
+}
+
+void Televisores::setResolucao(int resolucao){
+    _resolucao = resolucao;
 }
--- televisores.h
+++ televisores.h
@@ -5,7 +5,12 @@ private:
     int _tela;
     int _resolucao;
 public:
+    //Construtor
+    Televisores(int id, double preco, int quantidade, std::string marca, int tela, int resolucao);
     //Gets
     int getTela();
     int getResolucao();
+    //Sets
+    void setTela(int tela);
+    void setResolucao(int resolucao);
 };
