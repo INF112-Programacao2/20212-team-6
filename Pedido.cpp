9c62cc64cbadbd7ca334178e7dfb4ca8b73d960c
gabrielfernandesufv <91228353+gabrielfernandesufv@users.noreply.github.com>
2022-03-13 13:17:41 -0300

    Update Pedido.cpp
    Funções adicionais

--- Pedido.cpp
+++ Pedido.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <cstring>
-#include <algorithm>
+#include  <algorithm>
 #include <fstream>
+#include <string>
+#include <exception>
 #include "Pedido.h"
+#include "Produto.h"
 
 int Pedido::ph_id = -8192;
 Pedido::Pedido()
 {
-	std::cout << sizeof(quantidade) << std::endl;
+	//std::cout << sizeof(quantidade) << std::endl;
 	for (int x = 0;x < size;x++)
 	{
 		quantidade[x] = 0;
 		carrinho[x] = Produto(ph_id, 0,199);
 
 	}
+	p_size = 0;
 
 
 
@@ -35,6 +39,7 @@ void Pedido::add_produto(Produto& p)
 			{
 				carrinho[x] = p;
 				quantidade[x] = 1;
+				p_size++;
 				break;
 			}
 		}
@@ -56,7 +61,93 @@ double Pedido::valor_total()
 	return res;
 }
 
+
+void Pedido::limpar_carrinho()
+{
+	for (int x = 0;x < p_size;x++)
+	{
+		quantidade[x] = 0;
+		carrinho[x] = Produto(ph_id, 0, 199);
+
+	}
+}
+
+int Pedido::remover_item(std::string in)
+{
+	int inx = -1;
+	std::string nm = "";
+
+	try {
+		inx = std::stoi(in);
+	}
+	catch (std::invalid_argument e)
+	{
+		nm = in;
+	}
+	//std::cout << inx << std::endl;
+	inx--;
+	if (inx >= 0)
+	{
+		
+		if (inx > p_size -1)
+		{
+			std::cout << "\x1b[0;91m Valor maior que o numero de produtos no carrinho\x1b[0m" << std::endl;
+			return 1;
+		}
+
+		quantidade[inx] = 0;
+		carrinho[inx] = Produto(ph_id, 0, 199);
+
+		if (inx != p_size - 1)
+		{
+			Produto temp[100];
+			int temp_q[100] = {};
+			int idx=0;
+			for (int c = 0;c < p_size;c++)
+			{
+				if (carrinho[c].getID() != ph_id)
+				{
+					temp[idx] = carrinho[c];
+					temp_q[idx] = quantidade[c];
+					idx++;
+
+				}
+			}
+			limpar_carrinho();
+			for (int d = 0;d < idx;d++)
+			{
+				//add_produto(temp[d]);
+				carrinho[d] = temp[d];
+				quantidade[d] = temp_q[d];
+			}
+
+		}
+
+
+
+		
+	    p_size--;
+		return 0;
+	}
+
+	else {
+		std::cout << "\x1b[0;91mValor eh zero ou menor\x1b[0m" << std::endl;
+		return 2;
+	}
+
+	return 4;
+}
+
+
+void Pedido::listar_produtos()
+{
+	for (int c = 0;c < p_size;c++)
+	{
+		std::cout << carrinho[c].getPreco() << " x"  << quantidade[c] << std::endl;
+	}
+}
+
 Pedido::~Pedido()
 {
-
+	
 }
