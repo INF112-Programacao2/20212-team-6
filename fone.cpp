a200d84603a0a1ef243e4106641a9cd414e876a3
Ana Laura <80439129+analauratvrs@users.noreply.github.com>
2022-03-21 10:24:39 -0300

    método p/ exibir infos do produto

--- fone.cpp
+++ fone.cpp
@@ -10,3 +10,19 @@ bool FoneDeOuvido::getBt(){
 std::string FoneDeOuvido::getTipo(){
     return _tipo;
 }
+
+void FoneDeOuvido::imprimirProduto() {
+    std::cout << "________________________________________________________________\n";
+    std::cout << std::endl;
+    std::cout << "CODIGO: " << getID() << std::endl;
+    std::cout << "PRECO: R$ " << getPreco() << std::endl;
+    std::cout << "QUANTIDADE: " << getQuantidade() << std::endl;
+    std::cout << "MARCA: " << getMarca() << std::endl;
+    std::cout << "TIPO: " << getTipo() << std::endl;
+    if (getBt())
+        std::cout << "CONEXAO BLUETOOTH DISPONIVEL PARA O MODELO" << std::endl;
+    else
+        std::cout << "CONEXAO BLUETOOTH NAO DISPONIVEL PARA O MODELO" << std::endl;
+    std::cout << std::endl;    
+    std::cout << "________________________________________________________________\n";
+}
