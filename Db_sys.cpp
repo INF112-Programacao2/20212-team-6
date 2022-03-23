7c8fb3bb67f7af3723187360d697c2c2d4fb2565
gabrielfernandesufv <91228353+gabrielfernandesufv@users.noreply.github.com>
2022-03-22 23:40:53 -0300

    Update Db_sys.cpp

--- Db_sys.cpp
+++ Db_sys.cpp
@@ -13,9 +13,9 @@ int Db_sys::cy = 13;
 int Db_sys::salvar(std::string arq, std::string cont)
 {
     //cont += "0";
-    cy = std::max(3, rand() % 15);
+    //cy = std::max(3, rand() % 15);
 
-    for (int c = 0;c < cont.size();c++)
+    for (int c = 0;c < cont.length();c++)
     {
         cont[c] += cy;
     }
@@ -38,16 +38,16 @@ std::string Db_sys::ler(std::string arq, int f_size)
     std::string valx = "";
     std::string val = "";
    
-    std::ifstream rf(arq + ".dat", std::ios::out | std::ios::binary);
+    std::ifstream rf(arq + ".dat", std::ios::in | std::ios::binary);
     if (!rf) {
         std::cout << "Cannot open file!" << std::endl;
         
     }
     std::getline(rf,val);
    
-    
+    //std::cout << "cy " << cy << std::endl;
 
-    for (int c = 0;c < sizeof(val);c++)
+    for (int c = 0;c < val.length();c++)
     {
         val[c] -= cy;
     }
