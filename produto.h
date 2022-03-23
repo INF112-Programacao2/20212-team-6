c3c5e3986132601042a190bdb50b70194c1a0be1
Ana Laura <80439129+analauratvrs@users.noreply.github.com>
2022-03-23 17:06:20 -0300

    adição de atributo e seus métodos get/set

--- produto.h
+++ produto.h
@@ -9,14 +9,14 @@ private:
     double _preco;
     int _quantidade;
     bool _emEstoque;
-
+    std::string _descricao;
     std::string _marca;
 
 public:
     //Construtor
     Produto();
     Produto(int id, double preco, int quantidadem);
-    Produto(int id, double preco, int quantidadem, std::string marca);
+    Produto(int id, double preco, int quantidadem, std::string marca, std::string descricao);
     virtual void imprimirProduto();     // exibe informações do produto, será sobrescrita nas subclasses
 
     //Gets
@@ -24,13 +24,14 @@ public:
     double getPreco();
     int getQuantidade();
     bool getEmEstoque();
-
     std::string getMarca();
+    std::string getDescricao();
 
     //Sets
     void setEmEstoque();
-
+    void setQuantidade(int qtde);
     void setMarca(std::string marca);
+    void setDescricao(std::string descricao);
 
 };
 
