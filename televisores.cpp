d09586413b9a59ac71d786f622e137758c545963
Ana Laura <80439129+analauratvrs@users.noreply.github.com>
2022-03-23 17:24:49 -0300

    atualização

--- televisores.cpp
+++ televisores.cpp
@@ -1,7 +1,7 @@
 #include "televisores.h"
 
-Televisores::Televisores(int id, double preco, int quantidade, std::string marca, int tela, int resolucao):
-    Produto(id, preco, quantidade, marca), _tela(tela), _resolucao(resolucao) {}
+Televisores::Televisores(int id, double preco, int quantidade, std::string marca, std::string descricao, int tela, int resolucao):
+    Produto(id, preco, quantidade, marca, descricao), _tela(tela), _resolucao(resolucao) {}
 
 int Televisores::getTela(){
     return _tela;
@@ -20,14 +20,17 @@ void Televisores::setResolucao(int resolucao){
 }
 
 void Televisores::imprimirProduto() {
-    std::cout << "________________________________________________________________\n";
+    std::cout << "______________________________________________________________________\n";
+    if (!getEmEstoque())
+        std::cout << "\tATENÇÃO: PRODUTO FORA DE ESTOQUE!\n";
     std::cout << std::endl;
-    std::cout << "CODIGO: " << getID() << std::endl;
-    std::cout << "PRECO: R$" << getPreco() << std::endl;
-    std::cout << "QUANTIDADE: " << getQuantidade() << std::endl;
-    std::cout << "MARCA: " << getMarca() << std::endl;
-    std::cout << "TAMANHO DA TELA: " << getTela() << " POLEGADAS"<< std::endl;
-    std::cout << "RESOLUCAO: " << getResolucao() << " p"<< std::endl;
+    std::cout << "\tCODIGO: " << getID() << std::endl;
+    std::cout << "\tDESCRIÇÃO DO PRODUTO: " << getDescricao() << std::endl;
+    std::cout << "\tPRECO: R$" << getPreco() << std::endl;
+    std::cout << "\tQUANTIDADE: " << getQuantidade() << std::endl;
+    std::cout << "\tMARCA: " << getMarca() << std::endl;
+    std::cout << "\tTAMANHO DA TELA: " << getTela() << " POLEGADAS"<< std::endl;
+    std::cout << "\tRESOLUCAO: " << getResolucao() << " p"<< std::endl;
     std::cout << std::endl;    
-    std::cout << "________________________________________________________________\n";
+    std::cout << "______________________________________________________________________\n";
 }
