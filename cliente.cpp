62c7b2866722b55fa4086e159c3a84b8e9ab62a9
gabrielfernandesufv <91228353+gabrielfernandesufv@users.noreply.github.com>
2022-03-25 18:35:25 -0300

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -4,32 +4,31 @@
 #include "Pedido.h"
 #include "produto.h"
 
-Cliente::Cliente(std::string nome,  unsigned long long int cpf, int id_usuario,std::string endereco,  unsigned long long int telefone) 
-: Usuario (nome, cpf, id_usuario) {
+Cliente::Cliente(std::string nome, unsigned long long int cpf, int id_usuario, std::string endereco, unsigned long long int telefone)
+    : Usuario(nome, cpf, id_usuario) {
     this->endereco = endereco;
     this->telefone = telefone;
-    this->saldoEmConta = 1000;
 }
 
-// METODOS SET
+// MÉTODOS SET
 
 void Cliente::setNome(std::string Nome) {
-        this->_nome = Nome;
+    this->_nome = Nome;
 }
 
-void Cliente::setCPF( unsigned long long int CPF) {
-        this->_cpf = CPF;
+void Cliente::setCPF(unsigned long long int CPF) {
+    this->_cpf = CPF;
 }
 
 void Cliente::setID(int id) {
-        this->_id_usuario = id;
+    this->_id_usuario = id;
 }
 
 void Cliente::setEndereco(std::string Endereco) {
     this->endereco = Endereco;
 }
-    
-    
+
+
 void Cliente::setTelefone(unsigned long long int tel) {
     this->telefone = tel;
 }
@@ -38,24 +37,20 @@ void Cliente::setSelecao(int opc) {
     this->selecao = opc;
 }
 
-void Cliente::setP(Produto *p){
+void Cliente::setP(Produto* p) {
     this->p = p;
 }
 
-void Cliente::setRemove(std::string remove){
-    this->remove=remove;
+void Cliente::setRemove(std::string remove) {
+    this->remove = remove;
 }
 
-void Cliente::setSaldo(double saldo){
-    this->saldoEmConta=saldo-Carrinho.valor_total();
-}
-
-// METODOS GET
+// MÉTODOS GET
 std::string Cliente::getNome() {
     return _nome;
 }
 
- unsigned long long int Cliente::getCPF() {
+unsigned long long int Cliente::getCPF() {
     return _cpf;
 }
 
@@ -71,97 +66,73 @@ unsigned long long int Cliente::getTelefone() {
     return telefone;
 }
 
-int Cliente::getSelecao(){
+int Cliente::getSelecao() {
     return selecao;
 }
 
-Produto Cliente::getP(){
+Produto Cliente::getP() {
     return *p;
 }
 
-std::string Cliente::getRemove(){
+std::string Cliente::getRemove() {
     return remove;
-}
-
-double Cliente::getSaldo(){
-    return saldoEmConta;
 }
 // MOSTRA DADOS DO CLIENTE NA TELA
 
 void Cliente::exibirDados() {
     std::cout << "-------------------------------------------\n";
     std::cout << "DADOS DO CLIENTE\n";
-    std::cout << "NOME: "<< getNome() << std::endl;
-    std::cout << "CPF: "<< getCPF() << std::endl;
-    std::cout << "ID de usuario: "<< getID() << std::endl;
-    std::cout << "ENDEREÇO: "<< getEndereco() << std::endl;
-    std::cout << "TELEFONE: "<< getTelefone() << std::endl;
+    std::cout << "NOME: " << getNome() << std::endl;
+    std::cout << "CPF: " << getCPF() << std::endl;
+    std::cout << "ID de usuario: " << getID() << std::endl;
+    std::cout << "ENDEREÇO: " << getEndereco() << std::endl;
+    std::cout << "TELEFONE: " << getTelefone() << std::endl;
     std::cout << "-------------------------------------------\n";
 }
 //Aqui o cliente realiza uma nova compra
-void Cliente::meuCarrinho(){
-    
-    do{
-        menu_produtos:
+void Cliente::meuCarrinho() {
+
+    do {
+
         std::cout << "____________MEU CARRINHO____________\n\n";
         std::cout << "Prossiga selecionando uma das seguintes opcoes:\n";
         std::cout << "1) Visualizar meu carrinho\n";
         std::cout << "2) Adicionar itens\n";
         std::cout << "3) Remover itens\n";
         std::cout << "4) Limpar carrinho\n";
-        std::cout << "5) Concluir compra\n";
         std::cout << "_____________________________________\n";
-        std::cout << "Se deseja sair, digite 6\n";
+        std::cout << "Se deseja sair, digite 0\n";
         std::cin >> selecao;
 
         switch (selecao)
         {
-            case 1:
-            { 
-                Carrinho.listar_produtos();
-                std::cout <<"\n Valor total: " << Carrinho.valor_total();
-                break;
-            }
-            case 2:
-            {
-                Compras.imprimirProdutosDisponiveis();
-                *p = Compras.selecaoDeCompra();
-                Carrinho.add_produto(*p);
-                break;
-            }
-            case 3:
-            {
-                Carrinho.remover_item(remove);
-                break;
-            }
-            case 4:
-            {
-                Carrinho.limpar_carrinho();
-                break;
-            }
-            case 5:
-            {
-                std::cout <<"\n Valor total: " << Carrinho.valor_total();
-                std::cout << "Valor total em conta é R$ " << getSaldo()<< std::endl;
-                if(getSaldo()<Carrinho.valor_total())
-                {
-                    std::cout <<"Infelizmente você nao tem saldo suficiente para realizar essa compra.\n";
-                    std::cout <<"Se acredita que isso esteja errado, favor entrar em contato com um administrador (31 9999-9999).\n";
-                    break;
-                }
-                else
-                {
-                    setSaldo(saldoEmConta);
-                    std::cout << "O valor restante em conta é R$ " << getSaldo() << std::endl;
-                }
-            }
-            default:
-            {
-                goto menu_produtos;
-                break;
-            }
+        case 1:
+        {
+            Carrinho.listar_produtos();
+            std::cout << "\n Valor total: " << Carrinho.valor_total();
+            break;
         }
-    }while(selecao<7);
+        case 2:
+        {
+            Compras.imprimirProdutosDisponiveis();
+            Produto p = Compras.selecaoDeCompra();
+            Carrinho.add_produto(p);
+            break;
+        }
+        case 3:
+        {
+            Carrinho.remover_item(remove);
+            break;
+        }
+        case 4:
+        {
+            Carrinho.limpar_carrinho();
+            break;
+        }
+        default:
+            break;
+        }
+    } while (selecao != 0);
 
 
 
