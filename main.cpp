d44beba037dd19f29b41b5327c27e431b53ba75f
gabrielfernandesufv <91228353+gabrielfernandesufv@users.noreply.github.com>
2022-03-26 12:10:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <string>
 #include "includes.h"
+#include "Administrador.h"
+#include "Cliente.h"
 
 int main()
 {
@@ -12,21 +14,25 @@ int main()
     //Início do programa
     //chama a função de Menu Principal enquanto o login não for concedido.
     imprimeASCII();
-    try{
-        while(true){
-            if(menuPrincipal(isAdmin))
+    try {
+        while (true) {
+            if (menuPrincipal(isAdmin))
                 break;
         }
     }
-    catch(std::invalid_argument &e){
+    catch (std::invalid_argument& e) {
         std::cout << e.what();
         return 1;
     }
-    if(isAdmin){
-        //std::cout << "Inclua as funções do admin aqui.\n";
+    if (isAdmin) {
+        Administrador ad = Administrador("admin",12,0);
+        ad.menuAdm();
     }
-    else{
-        //std::cout << "Somente um usuario normal.\n";
+    else {
+        Cliente ct = Cliente("cliente",12,0,"",1);
+        Estoque::Criar_arquivo_estoque();
+        Estoque::Ler_arquivo_estoque();
+        ct.meuCarrinho();
     }
     return 0;
 }
