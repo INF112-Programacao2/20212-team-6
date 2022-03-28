4e088ca8369ad01a934c85a46a8dcca6ba2f49f7
gabrielfernandesufv <91228353+gabrielfernandesufv@users.noreply.github.com>
2022-03-28 11:36:11 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 
 int main()
 {
-   
+
     std::cout << "\x1b[1;7m" << '\r';
     system("clear");
     bool isAdmin = false;
@@ -30,29 +30,55 @@ int main()
         return 1;
     }
     if (isAdmin) {
-        
+
         Estoque::Criar_arquivo_estoque();
         Estoque::Ler_arquivo_estoque();
-        Administrador ad = Administrador("admin",12,0);
+        Administrador ad = Administrador("admin", 12, 0);
         ad.menuAdm();
     }
     else {
         std::string nome, endereco;
-        long long int telefone=0, cpf=0;
+        long long int telefone = -1, cpf = -1;
+        std::string cf="", tl = "";
         //Fornece as informacoes para criacao do cliente
         std::cout << "\tVamos completar o seu cadastro\n";
         std::cout << "\tPara comecar, digite seu nome completo:\n";
         std::cin.ignore();
         getline(std::cin, nome);
-        std::cout << "\tDigite seu cpf: (somente numeros)\n";
-        std::cin >> cpf;
+        while (cpf == -1)
+        {
+            std::cout << "\tDigite seu cpf: (somente numeros)\n";
+            std::cin >> cf;
+            try
+            {
+                cpf = std::stoll(cf);
+            }
+            catch (std::invalid_argument e)
+            {
+                std::cout << "Cpf invalido" << std::endl;
+            }
+            
+            
+        }
         std::cout << "\tDigite seu endereco e aperte 'enter' somente quando terminar: \n";
         std::cin.ignore();
         getline(std::cin, endereco);
-        std::cout <<"\tPara terminar, digite seu telefone: (somente numeros)\n";
-        std::cin >> telefone;
+        
+        while (telefone == -1)
+        {
+            std::cout << "\tPara terminar, digite seu telefone: (somente numeros)\n";
+            std::cin >> tl;
+            try
+            {
+                telefone = std::stoll(tl);
+            }
+            catch (std::invalid_argument e)
+            {
+                std::cout << "Telefone invalido" << std::endl;
+            }
+        }
 
-        Cliente ct = Cliente(nome,cpf,0,endereco,telefone);
+        Cliente ct = Cliente(nome, cpf, 0, endereco, telefone);
         //inicializa e le o estoque
         Estoque::Criar_arquivo_estoque();
         Estoque::Ler_arquivo_estoque();
@@ -61,5 +87,4 @@ int main()
     }
     return 0;
 }
-
 
